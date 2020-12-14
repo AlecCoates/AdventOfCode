ffa28c649efd2cb5b0918f92f1ade8783caa92d3
AlecCoates <42540348+AlecCoates@users.noreply.github.com>
2020-12-14 22:35:49 +0000

    Day 11

---
+++ 2020/Day11/script2.py
@@ -0,0 +1,50 @@
+from copy import deepcopy
+
+FLOOR = '.'
+SEAT_EMPTY = 'L'
+SEAT_OCCUPIED = '#'
+
+floor_plan = []
+with open("./input.txt", "r") as file:
+    for line in file.read().strip().split('\n'):
+        row = []
+        for char in line.strip():
+            row.append(char)
+        floor_plan.append(row)
+
+previous_floor_plan = []
+while floor_plan != previous_floor_plan:
+    new_floor_plan = deepcopy(floor_plan)
+    for i in range(len(floor_plan)):
+        for j in range(len(floor_plan[i])):
+            current_space = floor_plan[i][j]
+            if current_space == SEAT_EMPTY or current_space == SEAT_OCCUPIED:
+                adjacent_seats = 0
+                for a in range(-1, 2):
+                    for b in range(-1, 2):
+                        if 0 <= i+a < len(floor_plan):
+                            if 0<= j+b < len(floor_plan[i+a]):
+                                if not (a == 0 and b == 0):
+                                    c = 1
+                                    while 0 <= i+a*c < len(floor_plan) and 0 <= j+b*c < len(floor_plan[i+a*c]):
+                                        if floor_plan[i+a*c][j+b*c] == SEAT_OCCUPIED:
+                                            adjacent_seats += 1
+                                            break
+                                        elif floor_plan[i+a*c][j+b*c] == SEAT_EMPTY:
+                                            break
+                                        c += 1
+                if current_space == SEAT_EMPTY and adjacent_seats == 0:
+                    new_floor_plan[i][j] = SEAT_OCCUPIED
+                elif current_space == SEAT_OCCUPIED and adjacent_seats >= 5:
+                    new_floor_plan[i][j] = SEAT_EMPTY
+    previous_floor_plan = deepcopy(floor_plan)
+    floor_plan = deepcopy(new_floor_plan)
+
+count = 0
+for i in range(len(floor_plan)):
+    for j in range(len(floor_plan[i])):
+        if floor_plan[i][j] == SEAT_OCCUPIED:
+            count += 1
+
+print(count)
+input()
