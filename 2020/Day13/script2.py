ed30d71236fda0b64d947bf015b3ab0ab4f9cf0c
Alec Coates <42540348+AlecCoates@users.noreply.github.com>
2020-12-15 15:40:37 +0000

    Fix day 13 part 2
    Fixed logic for day 13 part 2.
    However, brute force won't work in any sort of reasonable time - another solution is needed.

--- 2020/Day13/script2.py
+++ 2020/Day13/script2.py
@@ -4,21 +4,23 @@ with open("./input.txt", "r") as f:
     line_split = f.readline().strip().split(',')
     for i in range(len(line_split)):
         if line_split[i] != 'x':
-            buses.append((int(line_split[i]), i))
+            buses.append((int(line_split[i]), i, int(line_split[i]) - i))
 
+bus_0_id = buses[0][0]
 found = False
-i = 1
+i = 0
 try:
     while not found:
+        i += 1
         found = True
-        timestamp = i * buses[0][0]
+        timestamp = i * bus_0_id
         for j in range(1, len(buses)):
-            if timestamp % buses[j][0] != buses[j][1]:
+            if timestamp % buses[j][0] != buses[j][2]:
                 found = False
                 break
-        i += 1
 except KeyboardInterrupt:
     print(" Checked up to:", i)
 
 print(i)
+print(i * buses[0][0])
 input()
